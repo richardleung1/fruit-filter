f48a7074ee1926f5978d9ed72b90824fa51274e9
richardleung1 <richardl57549@gmail.com>
2020-12-10 12:54:56 -0500

    add fruits

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import FruitContainer from './components/FruitContainer';
 
 function App() {
-  const fruits = ['banana', 'watermelon', 'apple', 'mango', 'orange']
+  const fruits = ['banana', 'watermelon', 'apple', 'mango', 'orange', 'kiwi', 'starfruit', 'crabapple', 'grape', 'grapefruit', 'mango', 'mangosteen']
   return (
     <div className="App">
       <FruitContainer fruits={fruits}/>
